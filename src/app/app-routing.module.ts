61312a3f32e0e6b5fdb7bb05754877d310e064dc
basicgts <kantinanm@nu.ac.th>
2022-06-02 17:52:13 +0700

    add home and edit route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules, NoPreloading } from '@angular/router';
 import { OsmComponent } from './page/osm/osm.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './login/login.component';
 import { SelectivePreloadingStrategyService } from "./customPreloader.service";
 
 const routes: Routes = [{
-  path:'',component:OsmComponent},
-  {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-  ,data: { preload: true, delay:5000 } }
+  path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:OsmComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { OsmComponent } from './page/osm/osm.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    OsmComponent
+    OsmComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
